b1b53960fda8e721e10c1d851e4bde5e35c72585
AaronZhang <461177348@qq.com>
2019-07-10 11:09:32 +0800

    Add files via upload

--- sacfile_rename.py
+++ sacfile_rename.py
@@ -42,14 +42,36 @@ def movefile_by_date(folder, dstFolder):
 				continue
 			
 			savePath = dstFolder + "\\" + matchObj.group(3)
-			print(matchObj.groups())
+			
+			print(os.path.join(root,file))
+			
 			if not os.path.exists(savePath):
 				os.makedirs(savePath) 
+			
+			if os.path.exists(savePath + "\\" + file):
+				print(savePath + "\\" + file)
+				continue
 				
 			shutil.copyfile(os.path.join(root,file), savePath + "\\" + file )
 			print(os.path.join(root,file), "  copyto  " + savePath + "\\" + file )
 			f.write( (os.path.join(root,file) + "  copyto  " + savePath + "\\" + file + "\n").encode() )
 	f.close()
+
+def rename_by_flodername(folder):
+	f = open("log.txt",'ab')
+	for root, dirs, files in os.walk(folder):
+		for file in files:
+			matchObj = re.match(r'^([^.]+).([EeNnZz]).([0-9]{8}).(sac)$', file)
+			
+			if matchObj == None:
+				continue
+			
+			newFileName = root.split("\\")[-1] + '.' + matchObj.group(2) + '.' + matchObj.group(3) +'.'+ matchObj.group(4)
+			os.rename( os.path.join(root,file), os.path.join(root,newFileName))
+			print( os.path.join(root,file), " => ", os.path.join(root,newFileName) )
+			f.write( (os.path.join(root,file) + " => "+ os.path.join(root,newFileName) + "\n").encode() )
+	f.close()
 	
-movefile_by_date("F:\\阳泉背景噪声\\yangquan_merge_r", "F:\\阳泉背景噪声\\阳泉背景噪声微震数据_SAC (copy)\\")
 #movefile_by_date("C:\\Users\\Aaron\\Desktop\\New folder", "C:\\Users\\Aaron\\Desktop\\阳泉背景噪声微震数据_SAC (copy)")
+#rename_by_flodername("F:\\阳泉背景噪声\\yangquan_merge_r")
+movefile_by_date("F:\\阳泉背景噪声\\yangquan_merge_r", "F:\\阳泉背景噪声\\阳泉背景噪声微震数据_SAC (copy)\\")
